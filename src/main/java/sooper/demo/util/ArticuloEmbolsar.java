0f78c3994a12cb1586e74acdee6093c7a2f738b2
jealma929 <jealmaformacion@gmail.com>
2025-04-14 11:46:46 +0200

    14042025

---
+++ src/main/java/sooper/demo/util/ArticuloEmbolsar.java
@@ -0,0 +1,47 @@
+package sooper.demo.util;
+
+public class ArticuloEmbolsar {
+
+		private int idArticulo;
+		private int idPedido;
+		private int volumen;
+		private int embolsado;
+		private int idContenedor;
+		
+		public ArticuloEmbolsar =
+		public Object articuloEmbolsar (int idArticulo, int idPedido, int volumen, int embolsado, int idContenedor) {
+			
+		}
+		public int getIdArticulo(){
+			return idArticulo;
+		}
+		public int getIdPedido() {
+			return idPedido;
+		}
+		public int getVolumen() {
+			return volumen;
+		}
+		public int getEmbolsado() {
+			return embolsado;
+		}
+		public int getIdContenedor() {
+			return idContenedor;
+		}
+		
+		public void setIdArticulo(int a) {
+			idArticulo = a;
+		}
+		public void setIdPedido(int p) {
+			idPedido=p;
+		}
+		public void setVolumen(int v) {
+			volumen=v;
+		}
+		public void setEmbolsado(int e) {
+			embolsado=e;
+		}
+		public void setIdContenedor(int c) {
+			idContenedor=c;
+		}
+
+}
